e674359fd3a03c17041d075234b115c4c1c6db1a
zzhang2293 <zzhang2293@outlook.com>
2022-10-09 22:07:44 -0500

    Update LeetCode334.java

--- LeetCode334.java
+++ LeetCode334.java
@@ -1,24 +1,24 @@
+import java.util.*;
 public class LeetCode334 {
-    public static boolean increasingTriplet(int[] nums) {
-        if (nums.length == 0 || nums.length == 1) return false;
-        int left = 0, right = 1, length = nums.length;
-        while (left + 2 < length) {
-            int current = nums[left];
-            int end = nums[right];
-            while (end > current) {
-                if (right - left >= 2) {
-                    return true;
-                }
-                current = nums[right];
-                right++;
-                if (right > length - 1) break;
-                end = nums[right];
-            }
-            left = right;
-            right++;
+    public static boolean hasBigger(int[] nums, int start, int val){
+        for ( int i = start; i < nums.length; i ++){
+            if ( nums[i] > val ) return true;
         }
         return false;
-
+    }
+    public static boolean increasingTriplet(int[] nums) {
+        if (nums.length < 3) return false;
+        int length = nums.length;
+        int first = nums[0], second = Integer.MAX_VALUE;
+        for (int i = 1; i < length; i ++){
+            if ( nums[i] > second ) return true;
+            else if (nums[i] > first && nums[i] < second) second = nums[i];
+            else {
+                first = nums[i];
+               // second = Integer.MAX_VALUE;
+            }
+        }
+        return false;
     }
 
     public static void main(String[] args) {
@@ -28,5 +28,6 @@ public class LeetCode334 {
         System.out.println(increasingTriplet(test2));
         int[] test3 = {1,3,2,4,3,5,4,6,5};
         System.out.println(increasingTriplet(test3));
+        System.out.println(increasingTriplet(new int[] {0,4,2,1,0,-1,-3}));
     }
 }
