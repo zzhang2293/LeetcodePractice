7735201b85e374ca7735cd556757c5e96629231d
zzhang <zzhang2293@outlook.com>
2023-04-06 23:48:41 -0500

    day7

--- day7/Solution.java
+++ day7/Solution.java
@@ -1,8 +1,6 @@
 package day7;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 class TreeNode {
      int val;
@@ -116,23 +114,81 @@ public class Solution {
         return money;
     }
 
-    public static int findTargetSumWaysHelper(int[] nums, int pointer, int current, int target){
+
+    public static int findTargetSumWaysHelper(Map<Pair, Integer> index, int[] nums, int pointer, int current, int target){
         int len = nums.length;
         if (pointer == len && current != target ) return 0;
         if (pointer == len) return  1;
-        return findTargetSumWaysHelper(nums, pointer + 1, current+nums[pointer], target)
-                + findTargetSumWaysHelper(nums, pointer+1, current-nums[pointer],target);
+        Pair pair = new Pair(pointer, current);
+        if (index.containsKey(pair)) {
+            System.out.println("true");
+            return index.get(pair);
+        }
+        int ways = findTargetSumWaysHelper(index, nums, pointer + 1, current+nums[pointer], target)
+                + findTargetSumWaysHelper(index, nums, pointer+1, current-nums[pointer],target);
+        index.put(pair, ways);
+        return ways;
 
 
     }
     public static int findTargetSumWays(int[] nums, int target){
-        return findTargetSumWaysHelper(nums, 0, 0, target);
+        int sum = Arrays.stream(nums).sum();
+        Map<Pair, Integer> maps = new HashMap<>();
+        return findTargetSumWaysHelper(maps, nums, 0, 0, target);
+    }
+
+    /**
+     *  #983
+     * @param days days need travel
+     * @param cost cost of different tickets
+     * @return minimum dollar
+     */
+    public static int minCostTickets(int[] days, int[] cost){
+        int[] index = new int[days.length];
+
+        return minCostHelper(index, days, cost, 0, 0);
+    }
+    private static int minCostHelper(int[] matrix, int[] days, int[] costs, int cover, int money){
+        int dayLen = days.length;
+        if (cover >= days[dayLen-1]) return money;
+        int index = -1;
+        for (int i = 0; i < dayLen; i ++){
+            if (days[i] <= cover) index = i;
+            else break;
+        }
+        if (matrix[index+1] != 0) return money + matrix[index+1];
+        int min = Integer.MAX_VALUE;
+        min = Math.min(min, minCostHelper(matrix, days, costs, days[index+1], costs[0]));
+        min = Math.min(min, minCostHelper(matrix, days, costs, days[index+1]+6, costs[1]));
+        min = Math.min(min, minCostHelper(matrix, days, costs, days[index+1]+29, costs[2]));
+        matrix[index+1] = min;
+        return min + money;
     }
 
     public static void main(String[] args) {
-//        System.out.println(numDistinct("rabbbit", "rabbit"));
-      //  System.out.println(maxProfit(new int[]{1,2,3,4,5}));
-        System.out.println(findTargetSumWays(new int[]{1}, 1));
+        System.out.println(minCostTickets(new int[]{1,4,6,7,8,20}, new int[]{2,7,15}));
     }
 
+}
+class Pair{
+    public int pointer;
+    public int value;
+    public Pair(){
+
+    }
+    public Pair(int pointer, int value){
+        this.pointer = pointer;
+        this.value = value;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        Pair other;
+        other = (Pair) obj;
+        return this.pointer == other.pointer && this.value == other.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.pointer;
+    }
 }
